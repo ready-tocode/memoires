5a066135cf8ece31cff89062b041fedb7f961b3d
Sanskar Srivastava <sanskar0703@gmail.com>
2022-10-30 16:02:52 +0530

    updated code

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,17 +1,3 @@
-// export default (posts = [], action) => {
-//     switch (action.type) {
-//         case 'FETCH_ALL':
-//             return action.payload;
-//         case 'CREATE':
-//             return [...posts, action.payload];
-//         case 'UPDATE':
-//             return posts.map((post) => post._id === action.payload._id ? action.payload : post );
-//         default:
-//             return posts;
-//     }
-// };
-
-
 import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
 
 export default (posts = [], action) => {
